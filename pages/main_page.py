2f13496c62e8c13268d306c5c3dd8b9ab7e45ea9
Lenar Saitov <lenarsaitov1@yandex.ru>
2020-12-28 02:04:58 +0300

    creating base

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,2 +1,9 @@
 import selenium
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from pages.locators import MainPageLocators
 
+class MainPage:
---
+++ tests/test_main_page.py
@@ -0,0 +1,2 @@
+def test_guest_should_see_login_and_registraiton_fields(browser):
+    page = LoginPage(browser, link)
